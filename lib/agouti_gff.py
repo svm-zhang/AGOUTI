404e910b88489ecc3f3966591ff56329296da0cc
Simo Zhang <zhangsimo850114@gmail.com>
2015-01-05 15:30:55 -0500

    initial commit

---
+++ lib/agouti_gff.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+class AGOUTI_GFF(object):
+	def __init__(self):
+		self.geneID = ""
+		self.geneStart = 0
+		self.geneStop = 0
+		self.program = ""
+		self.ctgID = ""
+		self.stopCodon = 0
+		self.startCodon = 0
+		self.strand = ''
+		self.lcds = []
+		self.link = 0
+
+	def setGene(self, geneID, start, stop, link):
+		self.geneID = geneID
+		self.geneStart = start
+		self.geneStop = stop
+		self.link = link
+
+	def setProgram(self, program):
+		self.program = program
+
+	def setContigID(self, ctgID):
+		self.ctgID = ctgID
+
+	def setStopCodon(self):
+		self.stopCodon = 1
+
+	def setStartCodon(self):
+		self.startCodon = 1
+
+	def setStrand(self, strand):
+		self.strand = strand
+
+	def updateCDS(self, cds_start, cds_stop):
+		self.lcds += [cds_start, cds_stop]
+
+	def debug(self):
+		print self.geneID, self.geneStart, self.geneStop
+		print self.ctgID
+		print self.strand
+
+	def getNumExons(self):
+		return len(self.lcds)/2
+
+	def is_fullGene(self):
+		if self.startCodon and self.stopCodon:
+			return True
+		else:
+			return False
+
