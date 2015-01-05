e74c09357b9320c18083385b3b272dc2e2476e67
Simo Zhang <zhangsimo850114@gmail.com>
2015-01-05 15:29:30 -0500

    repeats coding cleaned; add module path; add file output

--- src/updateGFF.py
+++ src/updateGFF.py
@@ -2,7 +2,11 @@ import os
 import sys
 import collections
 
-from agouti_gff import AGOUTI_GFF
+parpath = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), os.pardir))
+if os.path.exists(os.path.join(parpath, "lib", "__init__.py")):
+	sys.path.insert(0, parpath)
+
+from lib import agouti_gff as agff
 
 def func_getGeneModelstoFix(iGFFtoFix):
 	dGFFs = collections.defaultdict(list)
@@ -13,7 +17,7 @@ def func_getGeneModelstoFix(iGFFtoFix):
 				tmp_line = line.strip().split("\t")
 				if tmp_line[2] == "gene":
 					nGene += 1
-		lobj_GeneModels = [AGOUTI_GFF() for i in xrange(nGene)]
+		lobj_GeneModels = [agff.AGOUTI_GFF() for i in xrange(nGene)]
 		geneIndex = -1
 		link = 0
 		fGFF.seek(0)
@@ -55,7 +59,6 @@ def func_getScaffoldsPath(iEvidence, dGFFs, oGFF):
 
 	fOFIXGFF = open(oGFF, 'w')
 	bool_findPath = 0
-	first = 0
 	shift = 0
 	link = 0
 	ngeneAGOUTI = 0
@@ -77,11 +80,8 @@ def func_getScaffoldsPath(iEvidence, dGFFs, oGFF):
 					gapLen = 0
 					if len(tmp_line) == 4:
 						gapLen = int(tmp_line[-1][4:])
-					if nCtgs >= 1:
-						print ctgID, ctgLen, ctgStrand, gapLen, shift, nCtgs
-					if first == 0:
-						shift = 0
-						first = 1
+#					if nCtgs >= 1:
+#						print ctgID, ctgLen, ctgStrand, gapLen, shift, nCtgs
 					if ctgID in dGFFs:
 						if ctgStrand == 'r':
 							for i in range(len(dGFFs[ctgID])-1, -1, -1):
@@ -92,92 +92,40 @@ def func_getScaffoldsPath(iEvidence, dGFFs, oGFF):
 									update_strand = '-'
 								ori_geneStart = dGFFs[ctgID][i].geneStop
 								ori_geneStop = dGFFs[ctgID][i].geneStart
-
-								# this if/else should be merged as:
-								# update_geneStart = shift + (ctgLen-ori_geneStart+1)
-								# update_geneStop = shift + (ctgLen-ori_geneStop+1)
-								if shift == 0:
-									update_geneStart = ctgLen-ori_geneStart+1
-									update_geneStop = ctgLen-ori_geneStop+1
-								else:
-									update_geneStart = shift + (ctgLen-ori_geneStart+1)
-									update_geneStop = shift + (ctgLen-ori_geneStop+1)
-
-								# difference between in link or not is just the geneID
-								# so I think I should be able to fix this
+								update_geneStart = shift + (ctgLen-ori_geneStart+1)
+								update_geneStop = shift + (ctgLen-ori_geneStop+1)
 								if not dGFFs[ctgID][i].link:
 									link = 0
-									print ("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-										   %(scafID, update_geneStart, update_geneStop, update_strand,
-										   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+									fOFIXGFF.write("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+													%(scafID, update_geneStart, update_geneStop, update_strand,
+													dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 									for j in range(len(dGFFs[ctgID][i].lcds)-1, -2, -2):
 										ori_CDSstart = dGFFs[ctgID][i].lcds[j]
 										ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-										if shift == 0:
-											update_CDSstart = ctgLen-ori_CDSstart+1
-											update_CDSstop = ctgLen-ori_CDSstop+1
-										else:
-											update_CDSstart = shift + (ctgLen-ori_CDSstart+1)
-											update_CDSstop = shift + (ctgLen-ori_CDSstop+1)
-										print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-											   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+										update_CDSstart = shift + (ctgLen-ori_CDSstart+1)
+										update_CDSstop = shift + (ctgLen-ori_CDSstop+1)
+										fOFIXGFF.write("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+														%(scafID, update_CDSstart, update_CDSstop, update_strand,
+														dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 								else:
 									# merge gene model
 									if link == 0:
 										ngeneAGOUTI += 1
 										dGFFs[ctgID][i].geneID = "agouti_%d" %(ngeneAGOUTI)
-										print ("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-											   %(scafID, update_geneStart, update_geneStop, update_strand,
-											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+										fOFIXGFF.write("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+														%(scafID, update_geneStart, update_geneStop, update_strand,
+														dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 										link = 1
 									else:
 										dGFFs[ctgID][i].geneID = "agouti_%d" %(ngeneAGOUTI)
 									for j in range(len(dGFFs[ctgID][i].lcds)-1, -2, -2):
 										ori_CDSstart = dGFFs[ctgID][i].lcds[j]
 										ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-										if shift == 0:
-											update_CDSstart = ctgLen-ori_CDSstart+1
-											update_CDSstop = ctgLen-ori_CDSstop+1
-										else:
-											update_CDSstart = shift + (ctgLen-ori_CDSstart+1)
-											update_CDSstop = shift + (ctgLen-ori_CDSstop+1)
-										print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-											   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#									if link == 0:
-#										link = 1
-#										dGFFs[ctgID][i].geneID = "need to fix"
-#										print ("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-#											   %(scafID, update_geneStart, update_geneStop, update_strand,
-#											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#										for j in range(len(dGFFs[ctgID][i].lcds)-1, -2, -2):
-#											ori_CDSstart = dGFFs[ctgID][i].lcds[j]
-#											ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-#											if shift == 0:
-#												update_CDSstart = ctgLen-ori_CDSstart+1
-#												update_CDSstop = ctgLen-ori_CDSstop+1
-#											else:
-#												update_CDSstart = shift + (ctgLen-ori_CDSstart+1)
-#												update_CDSstop = shift + (ctgLen-ori_CDSstop+1)
-#											print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-#												   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-#												   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#									else:
-#										dGFFs[ctgID][i].geneID = "need to fix"
-#										for j in range(len(dGFFs[ctgID][i].lcds)-1, -2, -2):
-#											ori_CDSstart = dGFFs[ctgID][i].lcds[j]
-#											ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-#											if shift == 0:
-#												update_CDSstart = ctgLen-ori_CDSstart+1
-#												update_CDSstop = ctgLen-ori_CDSstop+1
-#											else:
-#												update_CDSstart = shift + (ctgLen-ori_CDSstart+1)
-#												update_CDSstop = shift + (ctgLen-ori_CDSstop+1)
-#											print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-#												   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-#												   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#										link = 0
+										update_CDSstart = shift + (ctgLen-ori_CDSstart+1)
+										update_CDSstop = shift + (ctgLen-ori_CDSstop+1)
+										fOFIXGFF.write("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+														%(scafID, update_CDSstart, update_CDSstop, update_strand,
+														dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 						elif ctgStrand == 'f':
 							for i in range(len(dGFFs[ctgID])):
 								if dGFFs[ctgID][i].strand == '-':
@@ -186,95 +134,44 @@ def func_getScaffoldsPath(iEvidence, dGFFs, oGFF):
 									update_strand = '+'
 								ori_geneStart = dGFFs[ctgID][i].geneStart
 								ori_geneStop = dGFFs[ctgID][i].geneStop
-
-								# this if/else should be merged as:
-								# update_geneStart = shift+ori_geneStart
-								# update_geneStop = shift+ori_geneStop
-								if shift == 0:
-									update_geneStart = ori_geneStart
-									update_geneStop = ori_geneStop
-								else:
-									update_geneStart = shift + ori_geneStart
-									update_geneStop = shift + ori_geneStop
-
+								update_geneStart = shift + ori_geneStart
+								update_geneStop = shift + ori_geneStop
 								if not dGFFs[ctgID][i].link:
 									link = 0
-									print ("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-										   %(scafID, update_geneStart, update_geneStop, update_strand,
-										   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+									fOFIXGFF.write("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+													%(scafID, update_geneStart, update_geneStop, update_strand,
+													dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 									for j in range(0, len(dGFFs[ctgID][i].lcds), 2):
 										ori_CDSstart = dGFFs[ctgID][i].lcds[j]
 										ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-										if shift == 0:
-											update_CDSstart = ori_CDSstart
-											update_CDSstop = ori_CDSstop
-										else:
-											update_CDSstart = shift + ori_CDSstart
-											update_CDSstop = shift + ori_CDSstop
-										print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-											   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+										update_CDSstart = shift + ori_CDSstart
+										update_CDSstop = shift + ori_CDSstop
+										fOFIXGFF.write("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+														%(scafID, update_CDSstart, update_CDSstop, update_strand,
+														dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 								else:
 									# merge gene models
 									if link == 0:
 										ngeneAGOUTI += 1
 										dGFFs[ctgID][i].geneID = "agouti_%d" %(ngeneAGOUTI)
-										print ("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-											   %(scafID, update_geneStart, update_geneStop, update_strand,
-											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+										fOFIXGFF.write("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+														%(scafID, update_geneStart, update_geneStop, update_strand,
+														dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 										link = 1
 									else:
 										dGFFs[ctgID][i].geneID = "agouti_%d" %(ngeneAGOUTI)
 									for j in range(0, len(dGFFs[ctgID][i].lcds), 2):
 										ori_CDSstart = dGFFs[ctgID][i].lcds[j]
 										ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-										if shift == 0:
-											update_CDSstart = ori_CDSstart
-											update_CDSstop = ori_CDSstop
-										else:
-											update_CDSstart = shift + ori_CDSstart
-											update_CDSstop = shift + ori_CDSstop
-										print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-											   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#									if link == 1:
-#										dGFFs[ctgID][i].geneID = "need to fix"
-#										for j in range(0, len(dGFFs[ctgID][i].lcds), 2):
-#											ori_CDSstart = dGFFs[ctgID][i].lcds[j]
-#											ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-#											if shift == 0:
-#												update_CDSstart = ori_CDSstart
-#												update_CDSstop = ori_CDSstop
-#											else:
-#												update_CDSstart = shift + ori_CDSstart
-#												update_CDSstop = shift + ori_CDSstop
-#											print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents%s"
-#												   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-#												   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#										link = 0
-#									else:
-#										link = 1
-#										dGFFs[ctgID][i].geneID = "need to fix"
-#										print ("%s\tAGOUTI\tgene\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s"
-#											   %(scafID, update_geneStart, update_geneStop, update_strand,
-#											   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
-#										for j in range(0, len(dGFFs[ctgID][i].lcds), 2):
-#											ori_CDSstart = dGFFs[ctgID][i].lcds[j]
-#											ori_CDSstop = dGFFs[ctgID][i].lcds[j-1]
-#											if shift == 0:
-#												update_CDSstart = ori_CDSstart
-#												update_CDSstop = ori_CDSstop
-#											else:
-#												update_CDSstart = shift + ori_CDSstart
-#												update_CDSstop = shift + ori_CDSstop
-#											print ("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents%s"
-#												   %(scafID, update_CDSstart, update_CDSstop, update_strand,
-#												   dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
+										update_CDSstart = shift + ori_CDSstart
+										update_CDSstop = shift + ori_CDSstop
+										fOFIXGFF.write("%s\tAGOUTI\tCDS\t%d\t%d\t.\t%s\t.\tID=%s.cds;Parents=%s\n"
+														%(scafID, update_CDSstart, update_CDSstop, update_strand,
+														dGFFs[ctgID][i].geneID, dGFFs[ctgID][i].geneID))
 					nCtgs -= 1
 					shift += ctgLen + gapLen
 					if nCtgs == 0:
 						bool_findPath = 0
-						first = 1
 						shift = 0
 	fOFIXGFF.close()
 
