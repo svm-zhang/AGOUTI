cba2410ad5068a0faddd0af0ca94630e5d44590c
Simo Zhang <zhangsimo850114@gmail.com>
2015-02-06 16:32:19 -0500

    move out functions, now this script handels only SAM to get contig pairs in dict

--- lib/agouti_sam.py
+++ lib/agouti_sam.py
@@ -4,12 +4,6 @@ import argparse
 import re
 import collections
 
-parpath = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), os.pardir))
-if os.path.exists(os.path.join(parpath, "lib", "__init__.py")):
-	sys.path.insert(0, parpath)
-
-from lib import agouti_gff as agff
-
 def getCIGAR(cigar):
 	tmp_cigar = re.split("([MIDNSHPX=])", cigar)[:-1]
 	alnLen = 0.0
@@ -47,17 +41,6 @@ def explainSAMFlag(samFlag):
 				duplicates = 1
 			samFlag -= bits[i]
 
-#	if selfStrand != mateStrand:
-#		return (paired, proper, selfUnMapped, mateUnMapped,
-#				"FR", secondAln, duplicates)
-#	else:
-#		if selfStrand == 1:
-#			return (paired, proper, selfUnMapped, mateUnMapped,
-#					"FF", secondAln, duplicates)
-#		else:
-#			return (paired, proper, selfUnMapped, mateUnMapped,
-#					"RR", secondAln, duplicates)
-
 	return (paired, proper, selfUnMapped, mateUnMapped,
 			selfStrand, mateStrand, secondAln, duplicates)
 
@@ -91,6 +74,8 @@ def sam2CtgPairs(isam, min_nLinks):
 		if pairA[0] == pairB[0] and contigA != contigB:
 			alnLenA = getCIGAR(pairA[5])
 			alnLenB = getCIGAR(pairB[5])
+			leftMostPosA = int(pairA[3])
+			leftMostPosB = int(pairB[3])
 			readLenA = len(pairA[9])
 			readLenB = len(pairB[9])
 			nMismatchesA = getMismatches(pairA[11:])
@@ -99,6 +84,10 @@ def sam2CtgPairs(isam, min_nLinks):
 			mapQB = int(pairB[4])
 			flagsA = explainSAMFlag(int(pairA[1]))
 			flagsB = explainSAMFlag(int(pairB[1]))
+			senseA = flagsA[4]
+			senseB = flagsB[4]
+#			print pairA
+#			print pairB
 #			print flagsA
 #			print flagsB
 #			print nMismatchesA, nMismatchesB
@@ -114,8 +103,6 @@ def sam2CtgPairs(isam, min_nLinks):
 				stopB = startB + int(alnLenB)
 #				startA, stopA = getMappedRegionOnContigs(int(pairA[3]), int(alnLenA), flagsA)
 #				startB, stopB = getMappedRegionOnContigs(int(pairB[3]), int(alnLenB), flagsB)
-				senseA = flagsA[4]
-				senseB = flagsB[4]
 				if contigA <= contigB:
 					if (contigA, contigB) not in dContigPairs:
 						dContigPairs[contigA, contigB] = [(startA, startB, stopA, stopB, senseA, senseB, pairA[0])]
@@ -144,74 +131,74 @@ def sam2CtgPairs(isam, min_nLinks):
 	sys.stderr.write("Number of Pairs of Contigs: %d\n" %(nCtgPairs))
 	return dContigPairs
 
-def gff2GeneModels(igff):
-	dGFFs = collections.defaultdict(list)
-	nGene = 0
-	with open(igff, 'r') as fIN:
-		for line in fIN:
-			if not line.startswith('#'):
-				tmp_line = line.strip().split("\t")
-				if tmp_line[2] == "gene":
-					nGene += 1
-		lobj_GeneModels = [agff.AGOUTI_GFF() for i in xrange(nGene)]
-		geneIndex = -1
-		fIN.seek(0)
-		for line in fIN:
-			if not line.startswith('#'):
-				tmp_line = line.strip().split("\t")
-				if tmp_line[2] == "gene":
-					geneIndex += 1
-					if geneIndex == 0:
-						lobj_GeneModels[geneIndex].setGene(tmp_line[8].split('=')[1],
-														   int(tmp_line[3]),
-														   int(tmp_line[4]), 0)
-					else:
-						preCtgID = lobj_GeneModels[geneIndex-1].ctgID
-						preGeneID = lobj_GeneModels[geneIndex-1].geneID
-						dGFFs[preCtgID].append(lobj_GeneModels[geneIndex-1])
-						lobj_GeneModels[geneIndex].setGene(tmp_line[8].split('=')[1],
-														   int(tmp_line[3]),
-														   int(tmp_line[4]), 0)
-					lobj_GeneModels[geneIndex].setProgram(tmp_line[1])
-					lobj_GeneModels[geneIndex].setContigID(tmp_line[0])
-					lobj_GeneModels[geneIndex].setStrand(tmp_line[6])
-				elif tmp_line[2] == "stop_codon":
-					lobj_GeneModels[geneIndex].setStopCodon()
-				elif tmp_line[2] == "start_codon":
-					lobj_GeneModels[geneIndex].setStartCodon()
-				elif tmp_line[2] == "CDS":
-					lobj_GeneModels[geneIndex].updateCDS(int(tmp_line[3]), int(tmp_line[4]))
-		dGFFs[lobj_GeneModels[geneIndex].ctgID].append(lobj_GeneModels[geneIndex])
+#def gff2GeneModels(igff):
+#	dGFFs = collections.defaultdict(list)
+#	nGene = 0
+#	with open(igff, 'r') as fIN:
+#		for line in fIN:
+#			if not line.startswith('#'):
+#				tmp_line = line.strip().split("\t")
+#				if tmp_line[2] == "gene":
+#					nGene += 1
+#		lobj_GeneModels = [agff.AGOUTI_GFF() for i in xrange(nGene)]
+#		geneIndex = -1
+#		fIN.seek(0)
+#		for line in fIN:
+#			if not line.startswith('#'):
+#				tmp_line = line.strip().split("\t")
+#				if tmp_line[2] == "gene":
+#					geneIndex += 1
+#					if geneIndex == 0:
+#						lobj_GeneModels[geneIndex].setGene(tmp_line[8].split('=')[1],
+#														   int(tmp_line[3]),
+#														   int(tmp_line[4]), 0)
+#					else:
+#						preCtgID = lobj_GeneModels[geneIndex-1].ctgID
+#						preGeneID = lobj_GeneModels[geneIndex-1].geneID
+#						dGFFs[preCtgID].append(lobj_GeneModels[geneIndex-1])
+#						lobj_GeneModels[geneIndex].setGene(tmp_line[8].split('=')[1],
+#														   int(tmp_line[3]),
+#														   int(tmp_line[4]), 0)
+#					lobj_GeneModels[geneIndex].setProgram(tmp_line[1])
+#					lobj_GeneModels[geneIndex].setContigID(tmp_line[0])
+#					lobj_GeneModels[geneIndex].setStrand(tmp_line[6])
+#				elif tmp_line[2] == "stop_codon":
+#					lobj_GeneModels[geneIndex].setStopCodon()
+#				elif tmp_line[2] == "start_codon":
+#					lobj_GeneModels[geneIndex].setStartCodon()
+#				elif tmp_line[2] == "CDS":
+#					lobj_GeneModels[geneIndex].updateCDS(int(tmp_line[3]), int(tmp_line[4]))
+#		dGFFs[lobj_GeneModels[geneIndex].ctgID].append(lobj_GeneModels[geneIndex])
+#
+#	nGeneModels = 0
+#	for k, v in sorted(dGFFs.items()):
+#		nGeneModels += len(v)
+#	sys.stderr.write("Number of Gene Models parsed: %d\n" %(nGeneModels))
+#	return dGFFs
 
-	nGeneModels = 0
-	for k, v in sorted(dGFFs.items()):
-		nGeneModels += len(v)
-	sys.stderr.write("Number of Gene Models parsed: %d\n" %(nGeneModels))
-	return dGFFs
-
-def matchGene(geneModels, start, stop):
-	exonIndex = 0
-	for i in xrange(len(geneModels)):
-		geneModel = geneModels[i]
-		# interval hits the gene
-		if start >= geneModel.geneStart and stop <= geneModel.geneStop:
-			geneIndex = i
-			return geneModel, geneIndex
-		# interval spans the gene
-		elif ((start >= geneModel.geneStart and start < geneModel.geneStop and
-			  stop > geneModel.geneStop) or (start < geneModel.geneStart and
-			  stop <= geneModel.geneStop and stop > geneModel.geneStart)):
-			geneIndex = i
-			return geneModel, geneIndex
-		# interval in between two genes
-		# this need to work on more
-		elif ((stop <= geneModel.geneStop and i == 0) or
-			  (start >= geneModel.geneStop and i == len(geneModels)-1) or
-			  (stop <= geneModel.geneStop and i == len(geneModels)-1) or
-			  (i > 0 and i < len(geneModels)-1 and
-			   start >= geneModels[i-1].geneStop and stop <= geneModel.geneStart)):
-			geneIndex = len(geneModels) - i - 1 + 1
-			return None, geneIndex
+#def matchGene(geneModels, start, stop):
+#	exonIndex = 0
+#	for i in xrange(len(geneModels)):
+#		geneModel = geneModels[i]
+#		# interval hits the gene
+#		if start >= geneModel.geneStart and stop <= geneModel.geneStop:
+#			geneIndex = i
+#			return geneModel, geneIndex
+#		# interval spans the gene
+#		elif ((start >= geneModel.geneStart and start < geneModel.geneStop and
+#			  stop > geneModel.geneStop) or (start < geneModel.geneStart and
+#			  stop <= geneModel.geneStop and stop > geneModel.geneStart)):
+#			geneIndex = i
+#			return geneModel, geneIndex
+#		# interval in between two genes
+#		# this need to work on more
+#		elif ((stop <= geneModel.geneStop and i == 0) or
+#			  (start >= geneModel.geneStop and i == len(geneModels)-1) or
+#			  (stop <= geneModel.geneStop and i == len(geneModels)-1) or
+#			  (i > 0 and i < len(geneModels)-1 and
+#			   start >= geneModels[i-1].geneStop and stop <= geneModel.geneStart)):
+#			geneIndex = len(geneModels) - i - 1 + 1
+#			return None, geneIndex
 #		for j in range(0, len(geneModel.lcds), 2):
 #			if start >= geneModel.lcds[j] and stop <= geneModel.lcds[j+1]:
 #				exonIndex = (j + 2) / 2
@@ -223,65 +210,62 @@ def matchGene(geneModels, start, stop):
 #			elif (start > geneModel.lcds[j+1] and		# cases where the interval
 #				  stop < geneModel.lcds[j+2]):			# spans exon/intron junctions
 #				return geneModel, exonIndex
-	print start, stop
-	sys.exit()
-	return None, -1
+#	print start, stop
+#	sys.exit()
+#	return None, -1
 
-def contigPairToGenePair():
-	pass
-
-def cleanContigPairs(dContigPairs, dGFFs):
-	for ctgPair, pairInfo in dContigPairs.items():
-		ctgA = ctgPair[0]
-		ctgB = ctgPair[1]
-		keep = []
-		print ">", ctgA, ctgB
-		for i in xrange(len(pairInfo)):
-			startA = pairInfo[i][0]
-			stopA = pairInfo[i][2]
-			startB = pairInfo[i][1]
-			stopB = pairInfo[i][3]
-			senseA = pairInfo[i][4]
-			senseB = pairInfo[i][5]
-			geneModelA, geneIndexA = None, -1
-			geneModelB, geneIndexB = None, -1
-			if ctgA in dGFFs:
-				print pairInfo[i]
-				geneModelA, geneIndexA = matchGene(dGFFs[ctgA], startA, stopA)
-			if ctgB in dGFFs:
-				geneModelB, geneIndexB = matchGene(dGFFs[ctgB], startB, stopB)
-			# case where gene models were found for a given pair of contigs
-			if geneModelA is not None and geneModelB is not None:
-				nGeneToRightA = len(dGFFs[ctgA]) - geneIndexA
-				nGeneToLeftA = geneIndexA
-				nGeneToLeftB = geneModelB
-				nGeneToRightB = len(dGFFs[ctgB]) - geneIndexB
-				# the following could be used as how to connect contigs
-				if nGeneToLeftA <= nGeneToRightA:
-					geneARelativePos = 5
-					nGeneSpannedA = nGeneToLeftA
-				else:
-					geneARelativePos = 3
-					nGeneSpannedA = nGeneToRightA
-				if nGeneToLeftB <= nGeneToRightB:
-					geneBRelativePos = 5
-					nGeneSpannedB = nGeneToLeftB
-				else:
-					geneBRelativePos = 3
-					nGeneSpannedB = nGeneToRightB
-				if nGeneSpannedA+nGeneSpannedB <= 1:
-					pass
-#					print "\t", "both", pairInfo[i]
-#					print "\t", "both", ctgA, ctgB, geneModelA.geneID, geneModelB.geneID, nGeneSpannedA, geneARelativePos, nGeneSpannedB, geneBRelativePos
-			# case where gene models were not found for a given pair of contigs
-			elif geneModelA is None and geneModelB is None:
-				print "\t", "none", pairInfo[i], geneIndexA, geneIndexB
-				pass
-			# case where either contig was found to match a gene model
-			# either geneModelA or geneModelB found a match
-			else:
-				pass
-		print
+#def cleanContigPairs(dContigPairs, dGFFs):
+#	for ctgPair, pairInfo in dContigPairs.items():
+#		ctgA = ctgPair[0]
+#		ctgB = ctgPair[1]
+#		keep = []
+#		print ">", ctgA, ctgB
+#		for i in xrange(len(pairInfo)):
+#			startA = pairInfo[i][0]
+#			stopA = pairInfo[i][2]
+#			startB = pairInfo[i][1]
+#			stopB = pairInfo[i][3]
+#			senseA = pairInfo[i][4]
+#			senseB = pairInfo[i][5]
+#			geneModelA, geneIndexA = None, -1
+#			geneModelB, geneIndexB = None, -1
+#			if ctgA in dGFFs:
+#				print pairInfo[i]
+#				geneModelA, geneIndexA = matchGene(dGFFs[ctgA], startA, stopA)
+#			if ctgB in dGFFs:
+#				geneModelB, geneIndexB = matchGene(dGFFs[ctgB], startB, stopB)
+#			# case where gene models were found for a given pair of contigs
+#			if geneModelA is not None and geneModelB is not None:
+#				nGeneToRightA = len(dGFFs[ctgA]) - geneIndexA
+#				nGeneToLeftA = geneIndexA
+#				nGeneToLeftB = geneModelB
+#				nGeneToRightB = len(dGFFs[ctgB]) - geneIndexB
+#				# the following could be used as how to connect contigs
+#				if nGeneToLeftA <= nGeneToRightA:
+#					geneARelativePos = 5
+#					nGeneSpannedA = nGeneToLeftA
+#				else:
+#					geneARelativePos = 3
+#					nGeneSpannedA = nGeneToRightA
+#				if nGeneToLeftB <= nGeneToRightB:
+#					geneBRelativePos = 5
+#					nGeneSpannedB = nGeneToLeftB
+#				else:
+#					geneBRelativePos = 3
+#					nGeneSpannedB = nGeneToRightB
+#				if nGeneSpannedA+nGeneSpannedB <= 1:
+#					pass
+##					print "\t", "both", pairInfo[i]
+##					print "\t", "both", ctgA, ctgB, geneModelA.geneID, geneModelB.geneID, nGeneSpannedA, geneARelativePos, nGeneSpannedB, geneBRelativePos
+#			# case where gene models were not found for a given pair of contigs
+#			elif geneModelA is None and geneModelB is None:
+#				print "\t", "none", pairInfo[i], geneIndexA, geneIndexB
+#				pass
+#			# case where either contig was found to match a gene model
+#			# either geneModelA or geneModelB found a match
+#			else:
+#				pass
+#		print
 
 	# I need to further filter contig pairs by the number of gene pairs supported by a minimum number of reads
 	# I need to figure out the orientation stuff
@@ -328,19 +312,18 @@ def cleanContigPairs(dContigPairs, dGFFs):
 #						keep.append(pairInfo[i])
 #		if len(keep) > 0:
 #			print keep
-	pass
 
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-gff", metavar="FILE", dest="igff", required=True, help="gene models in GFF format")
-	parser.add_argument("-mnl", metavar="INT", dest="min_nLinks", default=5, help="minimum number of reads supporting a link between a contig pair")
-	parser.add_argument("sam", nargs='?', help="reads mapping in SAM format")
-	args = parser.parse_args()
+#def main():
+#	parser = argparse.ArgumentParser()
+#	parser.add_argument("-gff", metavar="FILE", dest="igff", required=True, help="gene models in GFF format")
+#	parser.add_argument("-mnl", metavar="INT", dest="min_nLinks", default=5, help="minimum number of reads supporting a link between a contig pair")
+#	parser.add_argument("sam", nargs='?', help="reads mapping in SAM format")
+#	args = parser.parse_args()
+#
+#	dGFFs = gff2GeneModels(args.igff)
+#	dContigPairs = sam2CtgPairs(args.sam, args.min_nLinks)
+#	cleanContigPairs(dContigPairs, dGFFs)
 
-	dGFFs = gff2GeneModels(args.igff)
-	dContigPairs = sam2CtgPairs(args.sam, args.min_nLinks)
-	cleanContigPairs(dContigPairs, dGFFs)
-
-if __name__ == "__main__":
-	main()
+#if __name__ == "__main__":
+#	main()
 
