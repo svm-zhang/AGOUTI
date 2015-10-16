e177626c48c250964866bab3f08125838c3fb5d5
Simo Zhang <zhangsimo850114@gmail.com>
2015-10-16 11:59:57 -0400

    add -scaffold argument, and now can distinguish contigs and scaffods; move out get_initial_assembly to agouti_sequence.py

--- agouti.py
+++ agouti.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
-
 import os
 import sys
 import re
 import argparse
-import subprocess
+import logging
 
 agoutiBase = os.path.dirname(os.path.realpath(sys.argv[0]))
 sys.path.insert(1, agoutiBase)
 
+from src import agouti_sequence as agSeq
 from lib import agouti_sam as agBAM
 from src import agouti_filter_v2 as agFILTER
 #from src import test_filter as agFILTER
@@ -23,11 +22,15 @@ def parse_args():
 
 	parser = argparse.ArgumentParser(description=use_message)
 
-	parser.add_argument("-contig",
+	exclusiveGroup = parser.add_mutually_exclusive_group(required=True)
+	exclusiveGroup.add_argument("-contig",
 						metavar="FILE",
-						dest="contigFile",
-						required=True,
-						help="specify the initial assembly in FASTA format")
+						dest="contigFasta",
+						help="specify the assembly in contigs in FASTA format")
+	exclusiveGroup.add_argument("-scaffold",
+						metavar="FILE",
+						dest="scaffoldFasta",
+						help="specify the assembly in scaffolds in FASTA format")
 	parser.add_argument("-bam",
 						metavar="FILE",
 						type=argparse.FileType('r'),
@@ -40,66 +43,37 @@ def parse_args():
 						dest="gff",
 						required=True,
 						help="specify the predicted gene model in GFF format")
-	parser.add_argument("-out",
+	parser.add_argument("-outdir",
 						metavar="DIR",
 						dest="outDir",
 						default=".",
 						required=True,
 						help="specify the directory to store output files")
-	parser.add_argument("-mnl",
-						metavar="INT",
-						dest="minSupport",
-						default=5,
-						help="minimum number of joining reads pair supporting a link between a contig pair")
-	parser.add_argument("-nN",
-						metavar="INT",
-						dest="numNs",
-						default=1000,
-						help="number of Ns put in between a pair of contigs")
 	parser.add_argument("-p",
 						metavar="STR",
 						dest="prefix",
 						default="agouti",
+						help="specify the prefix for all output files [agouti]")
+	parser.add_argument("-mnl",
+						metavar="INT",
+						dest="minSupport",
+						default=5,
+						help="minimum number of joining reads pair supports [5]")
+	parser.add_argument("-nN",
+						metavar="INT",
+						dest="numNs",
+						default=1000,
+						help="number of Ns put in between a pair of contigs [1000]")
+	parser.add_argument("-debug",
+						action='store_true',
 						help="specify the output prefix")
 
 	args = parser.parse_args()
 	return args
 
-def get_initial_assembly(contigFile):
-	try:
-		fASSEMBLY = open(contigFile)
-	except IOError:
-		print "Error opening contig file: %s" % contigFile
-
-	sys.stderr.write("Reading the initial assembly ... ")
-	seq = ""
-	nameList = []
-	origSize = []
-	contigDict = {}
-	contigNum = 0
-	with open(contigFile, 'r') as fASSEMBLY:
-		for line in fASSEMBLY:
-			if line.startswith('>'):
-				if seq != "":
-					contigDict[contigNum] = seq
-					origSize.append(len(seq))
-					contigNum += 1
-					seq = ""
-				chrom = line.strip()[1:]
-				nameList.append(chrom)
-			else:
-				seq += line.strip()
-
-	contigDict[contigNum] = seq
-	origSize.append(len(seq))
-	fASSEMBLY.close()
-	sys.stderr.write("%d sequences parsed\n" %(len(contigDict)))
-	return nameList, contigDict, origSize
-
 def main():
 	args = parse_args()
 
-	contigFile = args.contigFile
 	bamFile = args.bamFile
 	gffFile = args.gff
 	prefix = args.prefix
@@ -107,7 +81,10 @@ def main():
 	if not os.path.exists(outDir):
 		os.makedirs(outDir)
 
-	nameList, contigDict, origSize = get_initial_assembly(contigFile)
+	if args.contigFasta:
+		seqNames, dSeq = agSeq.get_contigs(args.contigFasta)
+	elif args.scaffoldFasta:
+		seqNames, dSeq = agSeq.get_scaffolds(args.scaffoldFasta)
 
 	dGFFs = agGFF.get_gene_models(gffFile)
 
@@ -117,8 +94,8 @@ def main():
 #	dCtgPair2GenePair = agFILTER.map_contigPair2genePair(dContigPairs, dGFFs, joinPairsFile, args.minSupport)
 	dCtgPair2GenePair = agFILTER.cleanContigPairs(dContigPairs, dGFFs, joinPairsFile, args.minSupport)
 
-	pathList, edgeSenseDict, visitedDict = agSCAFF.agouti_scaffolding(len(contigDict), nameList, joinPairsFile, args.minSupport)
-	agUPDATE.agouti_update(pathList, contigDict, nameList,
+	pathList, edgeSenseDict, visitedDict = agSCAFF.agouti_scaffolding(seqNames, joinPairsFile, args.minSupport)
+	agUPDATE.agouti_update(pathList, dSeq, seqNames,
 						   edgeSenseDict, visitedDict, dGFFs,
 						   dCtgPair2GenePair, outDir, prefix, args.numNs)
 
