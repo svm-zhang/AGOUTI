7c13923647a7f12f4116061b06c58753e3f04a34
Simo Zhang <zhangsimo850114@gmail.com>
2015-10-18 22:36:47 -0400

    annotate fake gene as fake; allow reads mapped to both 5' and 3' gene models, and this require further efforts to merge the two gene models into one

--- src/agouti_filter_v2.py
+++ src/agouti_filter_v2.py
@@ -1,10 +1,11 @@
 import sys
 import os
 import collections
-import bisect
 import operator
+import logging
 
 from lib import agouti_gff as agGFF
+from lib import agouti_log as agLOG
 
 def merge_intervals(intervals):
 	if len(intervals) == 0:
@@ -26,10 +27,12 @@ def merge_intervals(intervals):
 
 def create_fake_genes(geneModels, index, ctg, intervals):
 	geneModel = agGFF.AGOUTI_GFF()
+	fake = 1
 	start = intervals[0][0]
 	stop = intervals[-1][1]
 	geneModel.setContigID = ctg
-	geneModel.setGene("%s_AGOUTI_fake_%d" %(ctg, len(geneModels)), start, stop)
+	geneModel.setGene("%s_AGOUTI_fake_%d" %(ctg, len(geneModels)),
+					  start, stop, fake)
 	geneModel.setProgram("AGOUTI")
 	geneModel.lcds = [pos for interval in intervals for pos in interval]
 	if index == 0 or index == -1:
@@ -40,6 +43,11 @@ def create_fake_genes(geneModels, index, ctg, intervals):
 		geneModels = geneModels[0:index] + [geneModel] + geneModels[index:]
 	print "\tcreate fake gene", geneModel.geneID, geneModel.geneStart, geneModel.geneStop, geneModel.lcds
 	return geneModels
+
+def find_interval_belongings(intervalQry, mergedIntervals):
+	for interval in mergedIntervals:
+		if intervalQry[1] >= interval[0] and interval[1] >= intervalQry[0]:
+			return interval
 
 def find_overlap(interval1, interval2):
 	if interval1[1] >= interval2[0] and interval2[1] >= interval1[0]:
@@ -84,6 +92,8 @@ def find_gene_overlap(interval, geneModel_5, geneModel_3):
 				return 1, 3
 			elif ovl_5 == 1 and ovl_3 == -1:
 				return -2, -2
+			elif ovl_5 == 0 and ovl_3 == 0:
+				return 0, 0
 
 def mapping_to_geneModel(geneModelsA, geneModelsB,
 						 mapIntervalsA, mapIntervalsB,
@@ -93,19 +103,14 @@ def mapping_to_geneModel(geneModelsA, geneModelsB,
 	geneModelA_3 = geneModelsA[-1]
 	geneModelB_5 = geneModelsB[0]
 	geneModelB_3 = geneModelsB[-1]
-#	mergedIntervalsA = merge_intervals(mapIntervalsA)
-#	mergedIntervalsB = merge_intervals(mapIntervalsB)
 	dMappingSupport = {}
 	dSenses = {}
+	print "mapping_to_geneModel all senses", collections.Counter(senses)
 	for i in xrange(len(mapIntervalsA)):
 		intervalA = mapIntervalsA[i]
-#		print "A find", intervalA, "belongings from", mergedIntervalsA
 		mergedIntervalA = find_interval_belongings(intervalA, mergedIntervalsA)
-#		print "A found belongings", mergedIntervalA
 		intervalB = mapIntervalsB[i]
-#		print "B find", intervalB, "belongings from", mergedIntervalsB
 		mergedIntervalB = find_interval_belongings(intervalB, mergedIntervalsB)
-#		print "B found belongings", mergedIntervalB
 		if (mergedIntervalA, mergedIntervalB) not in dMappingSupport:
 			dMappingSupport[mergedIntervalA, mergedIntervalB] = 1
 			dSenses[mergedIntervalA, mergedIntervalB] = [senses[i]]
@@ -132,12 +137,8 @@ def mapping_to_geneModel(geneModelsA, geneModelsB,
 			return None
 		elif geneIndexA != 0 and geneIndexB == 0:
 			return (geneIndexA, geneIndexB, endA, endB, [clusterA], [], dSenses[k[0]])
-#			createOnA.append((clusterA, clusterB))
-#			dIndex2Cluster[geneIndexA, geneIndexB, endA, endB].append((clusterA, clusterB))
 		elif geneIndexA == 0 and geneIndexB != 0:
 			return (geneIndexA, geneIndexB, endA, endB, [], [clusterB], dSenses[k[0]])
-#			createOnB.append((clusterA, clusterB))
-#			dIndex2Cluster[geneIndexA, geneIndexB, endA, endB].append((clusterA, clusterB))
 		elif geneIndexA != 0 and geneIndexB != 0:
 			createOnBoth.append((clusterA, clusterB))
 			dIndex2Cluster[geneIndexA, geneIndexB, endA, endB].append((clusterA, clusterB))
@@ -186,6 +187,7 @@ def get_genePair_for_contigPair(dGFFs, ctgA, ctgB, mapIntervalsA, mapIntervalsB,
 		return (0, 0, 0, 0, [], [], senses)
 	geneModelsA = dGFFs[ctgA]
 	geneModelsB = dGFFs[ctgB]
+	print "get_genePair_for_contigPair all senses", senses
 	genePair = mapping_to_geneModel(geneModelsA, geneModelsB,
 									mapIntervalsA, mapIntervalsB,
 									mergedIntervalsA, mergedIntervalsB,
@@ -193,17 +195,27 @@ def get_genePair_for_contigPair(dGFFs, ctgA, ctgB, mapIntervalsA, mapIntervalsB,
 	print "contig Pair", ctgA, ctgB, "genePair", genePair
 	return genePair
 
-def cleanContigPairs(dContigPairs, dGFFs, joinPairsFile, mnl):
-	sys.stderr.write("Filtering joining pairs ... \n")
+def set_module_name(name):
+	global moduleName
+	moduleName = name
+
+def enforce_filters(dContigPairs, dGFFs,
+					moduleOutDir, prefix, minSupport):
+	moduleProgressLogFile = os.path.join(moduleOutDir, "%s.agouti_filter.progressMeter" %(prefix))
+	moduleDebugLogFile = os.path.join(moduleOutDir, "%s.agouti_filter.debug" %(prefix))
+	moduleOutputFile = os.path.join(moduleOutDir, "%s.agouti.join_pairs.filtered.txt" %(prefix))
+	moduleProgressLogger = agLOG.AGOUTI_LOG(moduleName).create_logger(moduleProgressLogFile)
+	moduleDEBUGLogger = agLOG.AGOUTI_DEBUG_LOG(moduleName+"_DEBUG").create_logger(moduleDebugLogFile)
+
+	moduleProgressLogger.info("[BEGIN] Filtering joining pairs")
 	dCtgPair2GenePair = collections.defaultdict()
 	dMappedPos = collections.defaultdict()
 	daddedModels = collections.defaultdict(list)
-	nFewLinks = 0
-	fOUT = open(joinPairsFile, 'w')
+	nFiltered = 0
+	fOUT = open(moduleOutputFile, 'w')
 	for ctgPair, pairInfo in dContigPairs.items():
-		if len(pairInfo) < mnl:
+		if len(pairInfo) < minSupport:
 			del dContigPairs[ctgPair]
-			nFewLinks += 1
 			continue
 		ctgA = ctgPair[0]
 		ctgB = ctgPair[1]
@@ -213,12 +225,14 @@ def cleanContigPairs(dContigPairs, dGFFs, joinPairsFile, mnl):
 		mapIntervalsB = []
 		pairs = []
 		senses = []
+		keep = 0
 		for i in xrange(len(pairInfo)):
 			startA, startB, stopA, stopB, senseA, senseB, readID = pairInfo[i]
 			mapIntervalsA += [(startA, stopA)]
 			mapIntervalsB += [(startB, stopB)]
 			pairs += [(startA, stopA, startB, stopB)]
 			senses += [(senseA, senseB)]
+		print "enforce_filters all senses", senses
 		genePair = get_genePair_for_contigPair(dGFFs, ctgA, ctgB, mapIntervalsA, mapIntervalsB, senses)
 		geneModelsA = dGFFs[ctgA]
 		geneModelsB = dGFFs[ctgB]
@@ -231,57 +245,98 @@ def cleanContigPairs(dContigPairs, dGFFs, joinPairsFile, mnl):
 				if geneIndexB == -1 and (endB == 5 or endB == 0):
 					dGFFs[ctgB] = create_fake_genes(geneModelsB, 0, ctgB, intervalsB)
 					geneIndexB = 0
+					endB = 5
 				elif geneIndexB == 1 and (endB == 3 or endB == 0):
 					dGFFs[ctgB] = create_fake_genes(geneModelsB, len(geneModelsB), ctgB, intervalsB)
 					geneIndexB = len(dGFFs[ctgB]) - 1
+					endB = 3
+			else:
+				if endB == 0:
+					endB = 5
+				elif endB == 3:
+					geneIndexB = len(dGFFs[ctgB])-1
 			if geneIndexA != 0:
 				# create gene model according to endA using intervalsA
 				if geneIndexA == -1 and (endA == 5 or endA == 0):
 					dGFFs[ctgA] = create_fake_genes(geneModelsA, 0, ctgA, intervalsA)
 					geneIndexA = 0
+					endA = 5
 				elif geneIndexA == 1 and (endA == 3 or endA == 0):
 					dGFFs[ctgA] = create_fake_genes(geneModelsA, len(geneModelsA), ctgA, intervalsA)
 					geneIndexA = len(dGFFs[ctgA]) - 1
-			if endA == 3:
-				geneIndexA = len(dGFFs[ctgA]) - 1
-			if endB == 3:
-				geneIndexB = len(dGFFs[ctgB]) - 1
+					endA = 3
+			else:
+				if endA == 0:
+					endA = 3
+				elif endA == 3:
+					geneIndexA = len(dGFFs[ctgA])-1
 			print "contig Pair", ctgA, ctgB, "genePair", genePair
-			geneA = dGFFs[ctgA][geneIndexA]
-			geneB = dGFFs[ctgB][geneIndexB]
-			dCtgPair2GenePair[ctgA, ctgB] = [geneA, geneB]
-			sense = sensesCounter.keys()[0]
-			print "geneA", geneA.geneID, geneA.geneStart, geneA.geneStop
-			print "geneB", geneB.geneID, geneB.geneStart, geneB.geneStop
+			print "# model on ctgA", len(dGFFs[ctgA]), "# model on ctgB", len(dGFFs[ctgB])
+			print "geneIndexA", geneIndexA, "endA", endA, "geneIndexB", geneIndexB, "endB", endB
+			sense = sorted(sensesCounter.items(), key=operator.itemgetter(1), reverse=True)[0][0]
 			print "sense", sense
-			senseA = sense[0]
-			senseB = sense[1]
-			for i in xrange(len(pairInfo)):
-				startA, startB, stopA, stopB, senseA, senseB, readID = pairInfo[i]
-				intervalA = (startA, stopA)
-				intervalB = (startB, stopB)
-				print "intervalA", intervalA, "intervalB", intervalB
-				if len(intervalsA) == 0:
-					if len(intervalsB) == 0:
-						print "use all"
-						fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
+			if (geneIndexA == len(dGFFs[ctgA])-1 and endA == 3) and \
+			   (geneIndexB == 0 and endB == 5) and sense == ('+', '-'):
+					# FR + 3'-5'
+					keep = 1
+			elif (geneIndexA == 0 and endA == 5) and \
+				 (geneIndexB == 0 and endB == 5) and sense == ('-', '-'):
+					# RR + 5'-5'
+					keep = 1
+			elif (geneIndexA == len(dGFFs[ctgA])-1 and endA == 3) and \
+				 (geneIndexB == len(dGFFs[ctgB])-1 and endB == 3) and \
+				 sense == ('+', '+'):
+					# FF + 3'-3'
+					keep = 1
+			elif (geneIndexA == 0 and endA == 5) and \
+				 (geneIndexB == len(dGFFs[ctgB])-1 and endB == 3) and \
+				 sense == ('-', '+'):
+					# RF + 5'-3'
+					keep = 1
+			elif (geneIndexA == 0 and (endA == 0 or endA == 3)) and \
+				 (geneIndexB == 0 and (endB == 0 or endB == 5)) and \
+				 sense == ('+', '-'):
+					# only one gene on the contig
+					# it doesn't matter which end
+					keep = 1
+			print "****keep", keep
+			if keep:
+				geneA = dGFFs[ctgA][geneIndexA]
+				geneB = dGFFs[ctgB][geneIndexB]
+				dCtgPair2GenePair[ctgA, ctgB] = [geneA, geneB]
+				print "geneA", geneA.geneID, geneA.geneStart, geneA.geneStop
+				print "geneB", geneB.geneID, geneB.geneStart, geneB.geneStop
+				print "sense", sense
+				senseA = sense[0]
+				senseB = sense[1]
+				for i in xrange(len(pairInfo)):
+					startA, startB, stopA, stopB, _, _, readID = pairInfo[i]
+					intervalA = (startA, stopA)
+					intervalB = (startB, stopB)
+					print "intervalA", intervalA, "intervalB", intervalB
+					if len(intervalsA) == 0:
+						if len(intervalsB) == 0:
+							print "use all"
+							fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
+						else:
+							print "use all A, not all B"
+							overlap = find_overlap(intervalB, (geneB.geneStart, geneB.geneStop))
+							if overlap == 0:
+								fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
 					else:
-						print "use all A, not all B"
-						overlap = find_overlap(intervalB, (geneB.geneStart, geneB.geneStop))
-						if overlap == 0:
-							fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
-				else:
-					if len(intervalsB) == 0:
-						print "use all B, not all A"
-						overlap = find_overlap(intervalA, (geneA.geneStart, geneA.geneStop))
-						if overlap == 0:
-							fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
-					else:
-						print "not all Both"
-						overlapA = find_overlap(intervalA, (geneA.geneStart, geneA.geneStop))
-						overlapB = find_overlap(intervalB, (geneB.geneStart, geneB.geneStop))
-						if overlapA == 0 and overlapB == 0:
-							fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
+						if len(intervalsB) == 0:
+							print "use all B, not all A"
+							overlap = find_overlap(intervalA, (geneA.geneStart, geneA.geneStop))
+							if overlap == 0:
+								fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
+						else:
+							print "not all Both"
+							overlapA = find_overlap(intervalA, (geneA.geneStart, geneA.geneStop))
+							overlapB = find_overlap(intervalB, (geneB.geneStart, geneB.geneStop))
+							if overlapA == 0 and overlapB == 0:
+								fOUT.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(readID, ctgA, startA, senseA, ctgB, startB, senseB))
+			else:
+				nFiltered += 1
 #			if len(sensesCounter) == 1:
 #				sense = sensesCounter.keys()[0]
 #			else:
@@ -291,4 +346,7 @@ def cleanContigPairs(dContigPairs, dGFFs, joinPairsFile, mnl):
 #				ratio = float(senses[0][1])/(senses[0][1]+senses[1][1])
 #				print "ratio", ratio
 	fOUT.close()
-	return dCtgPair2GenePair
+	moduleProgressLogger.info("%d contig pairs filtered" %(nFiltered))
+	moduleProgressLogger.info("Succeeded")
+	return dCtgPair2GenePair, moduleOutputFile
+
