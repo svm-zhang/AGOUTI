ae786c2ce7d0f1658783469353be3ab80aab0912
Simo Zhang <zhangsimo850114@gmail.com>
2015-10-21 16:25:54 -0400

    minor changes

--- agouti.py
+++ agouti.py
@@ -154,13 +154,13 @@ def main():
 	moduleOutDir = os.path.join(outDir, "rnapathSTAR")
 	if not os.path.exists(moduleOutDir):
 		os.makedirs(moduleOutDir)
-	pathList, edgeSenseDict, visitedDict = agSCAFF.rnapathSTAR(seqNames, joinPairsFile, moduleOutDir, prefix, args.minSupport)
+	scafPaths, edgeSenseDict, visitedDict = agSCAFF.rnapathSTAR(seqNames, joinPairsFile, moduleOutDir, prefix, args.minSupport)
 
 	agUPDATE.set_module_name("AGOUTI_UPDATE")
 	moduleOutDir = os.path.join(outDir, "agouti_update")
 	if not os.path.exists(moduleOutDir):
 		os.makedirs(moduleOutDir)
-	agUPDATE.agouti_update(pathList, dSeq, seqNames,
+	agUPDATE.agouti_update(scafPaths, dSeq, seqNames,
 						   edgeSenseDict, visitedDict, dGFFs,
 						   dCtgPair2GenePair, outDir, prefix,
 						   moduleOutDir, args.numNs)
