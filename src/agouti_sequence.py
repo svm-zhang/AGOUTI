1df0a4742c5535ded0dbeb22eda1a206337d59be
Simo V. Zhang <simozhan@indiana.edu>
2015-12-22 21:59:17 -0500

    now split the header line by white spaces, the sequence ID is the first element of the split

--- src/agouti_sequence.py
+++ src/agouti_sequence.py
@@ -24,7 +24,7 @@ def agouti_seq_main(assemblyFile, outDir, prefix, debug=0):
 	for header, seq in read_fasta(assemblyFile):
 		# split header on any non-alphabetic character
 		# use only the first of the return list
-		header = re.split("\W+", header)[0]
+		#header = re.split("\W+", header)[0]
 		if header not in dHeaders:
 			contigs.append(header)
 			dSeqs[seqIndex] = seq
@@ -32,8 +32,8 @@ def agouti_seq_main(assemblyFile, outDir, prefix, debug=0):
 			dHeaders[header] = 1
 			seqLens.append(len(seq))
 		else:
-			agSeqProgress.error("AGOUTI found DUPLICATED header: %s" %(header))
-			agSeqProgress.error("QUIT")
+			agSeqProgress.logger.error("AGOUTI found DUPLICATED header: %s" %(header))
+			agSeqProgress.logger.error("QUIT")
 			sys.exit(1)
 
 	n50 = get_assembly_NXX(seqLens)
@@ -52,7 +52,8 @@ def read_fasta(assemblyFile):
 	with open(assemblyFile, 'r') as fASSEMBLY:
 		seqIter = (k[1] for k in itertools.groupby(fASSEMBLY, lambda line: line[0]== ">"))
 		for header in seqIter:
-			header = re.split("\W+", header.next()[1:].strip())[0]
+			#header = re.split("\W+", header.next()[1:].strip())[0]
+			header = header.next()[1:].strip().split()[0]
 			seq = "".join(s.strip() for s in seqIter.next())
 			yield header, seq
 
@@ -151,10 +152,10 @@ def rc_seq(seq):
 		return the reverse and complementary of
 		the give sequence
 	"""
-	alphabetsInString = "ACGTNTGCANacgtntgcan"
-	alphabets = { alphabetsInString[i]:alphabetsInString[i+5] for i
-				  in range(20)
-				  if i<5 or 10<=i<=14 }
+	alphabetsInString = "ACGTNRMKWYSBDHVTGCANRMKWYSBDHVacgtnrmkwysbdhvtgcanrmkwysbdhv"
+	alphabets = { alphabetsInString[i]:alphabetsInString[i+15] for i
+				  in range(60)
+				  if i<15 or 30<=i<=44 }
 	return "".join([alphabets[base] for base in seq[::-1]])
 
 def get_assembly_NXX(seqLens, nXX=50):
